81203a1b04dfc1dc9ef7706afb9bc9259cf224fb
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2024-10-07 07:55:36 +0200

    Added some columns to the Person class

--- src/main/java/tree/model/Person.java
+++ src/main/java/tree/model/Person.java
@@ -1,10 +1,15 @@
 package tree.model;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -18,6 +23,26 @@ public class Person {
 	@Column(name = "family_name")
 	private String familyName;
 	
-	@Column(name = "first_name")
-	private String firstName;
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "middle_name")
+	private String middleName;
+	
+	@Column(name = "Birthday")
+	private Date dateOfBirth;
+	
+	@Column(name = "Day_of_Death")
+	private Date dateOfDeath;
+	
+	@Column
+	private String profession;
+	
+	@Column
+	private String description;
+	
+	@OneToMany( mappedBy="parent", cascade = CascadeType.ALL)
+	private List <Person> children;
+	
+	
 }
